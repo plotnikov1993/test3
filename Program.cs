a2a2e70a0f9eb7a77ec1f1c3d7ed561f3c595c98
Андрей Плотников <plotnikov_andrew@icloud.com>
2022-09-08 00:35:54 +0300

    3

--- Program.cs
+++ Program.cs
@@ -5,3 +5,13 @@ for (int i = 0; i < array.Length; i++)
 {
     Console.Write(array[i] + " ");
 }
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+}
+   Zadacha();
